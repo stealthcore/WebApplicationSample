ecaf7ab75db8971ab098af4d4f697939cf06a004
Nathaniel St Elme <stealthcoretech@gmail.com>
2022-03-20 12:05:45 -0400

    First Commit

--- WebApplicationSample/App_Start/BundleConfig.cs
+++ WebApplicationSample/App_Start/BundleConfig.cs
@@ -24,7 +24,7 @@ namespace WebApplicationSample
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/site.css")); 
         }
     }
 }
