5e83e4d6af7c1c052d8b865c6ef8a5f2633ea119
Nathaniel St Elme <stealthcoretech@gmail.com>
2022-03-23 13:39:15 -0400

    2nd commit

--- WebApplicationSample/Controllers/StudentController.cs
+++ WebApplicationSample/Controllers/StudentController.cs
@@ -3,15 +3,28 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using WebApplicationSample.Models;
 
 namespace WebApplicationSample.Controllers
 {
     public class StudentController : Controller
     {
+
+        static IList<Student> studentList = new List<Student>{
+                new Student() { StudentId = 1, StudentName = "John", Age = 18 } ,
+                new Student() { StudentId = 2, StudentName = "Steve",  Age = 21 } ,
+                new Student() { StudentId = 3, StudentName = "Bill",  Age = 25 } ,
+                new Student() { StudentId = 4, StudentName = "Ram" , Age = 20 } ,
+                new Student() { StudentId = 5, StudentName = "Ron" , Age = 31 } ,
+                new Student() { StudentId = 4, StudentName = "Chris" , Age = 17 } ,
+                new Student() { StudentId = 4, StudentName = "Rob" , Age = 19 }
+            };
+
         // GET: Student
         public ActionResult Index()
         {
-            return View();
+            return View(studentList);
         }
     }
+
 }
